4bd12a8315a57d28927ed378e7a99e28daccfd88
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-07-06 19:32:10 -0400

    Update hands-on-3-7.js

--- hands-on-3-7.js
+++ hands-on-3-7.js
@@ -1,1 +1,26 @@
 
+/**
+ * Practice: Building objects
+ *
+ * - Create JavaScript objects based on objects in your current environment.
+ * - Give each object an identifiable name.
+ * - Create properties to describe the objects and set their values.
+ * - Find an object that has another object inside of it to create a nested object.
+ * - Test your objects in the browser console by accessing the entire object and its specific properties.
+ */
+
+const makeupBag = {
+    color: 'black',
+    makeUps:{
+        foundation: 'koreanBrand',
+        powder: 'japaneseBrand'
+    },
+    bagSize: {
+        length: 15,
+        diameter: 6,
+    },
+    newMakeUps: function(makeUp) {
+        this.makeUps.mascara = Mabline;
+        this.makeUps.lipStick = Mac;
+    },
+};
