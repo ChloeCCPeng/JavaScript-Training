633ab723e04389bcc8100ce5dc2ed0293c47cc47
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-07-07 19:23:38 -0400

    Update hands-on-3-14.js

--- hands-on-3-14.js
+++ hands-on-3-14.js
@@ -1,16 +1,34 @@
-class Book {
-    constructor (title, author, ISBN, pageNumber, currentPage, readStatus) {
-        this.title = title;
-        this.author = author;
-        this.ISBN = ISBN;
-        this.pageNumber = pageNumber;
-        this.currentPage = currentPage;
-        this.readStatus = readStatus;
-    }
-    updateCurrentPage (newCurrent) {
+// class Book {
+//     constructor (title, author, ISBN, pageNumber, currentPage, readStatus) {
+//         this.title = title;
+//         this.author = author;
+//         this.ISBN = ISBN;
+//         this.pageNumber = pageNumber;
+//         this.currentPage = currentPage;
+//         this.readStatus = readStatus;
+//     }
+//     updateCurrentPage (newCurrent) {
+//         this.currentPage = newCurrent;
+//     };
+//     updateReadStatus (newStatus) {
+//         this.readStatus = newStatus
+//     }
+// }
+
+// export default Book;
+
+function Book (title, author, ISBN, pageNumber, currentPage, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.pageNumber = pageNumber;
+    this.currentPage = currentPage;
+    this.readStatus = readStatus;
+
+    this.updateCurrentPage = function(newCurrent) {
         this.currentPage = newCurrent;
     };
-    updateReadStatus (newStatus) {
+    this.updateReadStatus = function(newStatus) {
         this.readStatus = newStatus
     }
 }
