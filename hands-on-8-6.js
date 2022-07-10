98315aa126a7c9a5336892d4282c1e2366770ac3
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-07-10 18:18:26 -0400

    Update hands-on-8-6.js

--- hands-on-8-6.js
+++ hands-on-8-6.js
@@ -6,6 +6,19 @@
  * - Helper function creates.
  */
 
+ const addIntro = (intro) => {
+    const newIntro = document.createElement('div');
+    newIntro.innerHTML = `
+    <h2>I am ${intro.name}</h2>
+    <h2>${intro.favorite} is my favorite country to visit</h2>
+    `;
+    return newIntro;
+};
+
+const intro = {
+    name: 'Chloe',
+    favorite: 'Iceland'
+};
 
 const addDiv = (div) => {
     const newDiv = document.createElement('div');
@@ -22,6 +35,14 @@ const div1 = {
     description: 'Travel through the world',
     picture: 'https://wwwnc.cdc.gov/travel/images/hiker-ocean-extreme.jpg',
 }
+const div2 = {
+    title: 'Travel2',
+    description: 'Travel through the world',
+    picture: 'https://wwwnc.cdc.gov/travel/images/hiker-ocean-extreme.jpg',
+}
 
 const main = document.querySelector('main');
+main.appendChild(addIntro(intro))
 main.appendChild(addDiv(div1))
+main.appendChild(addDiv(div2))
+
