ca5698b2ff770b77f8f561a4c8199e10a3371433
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-07-14 00:15:40 -0400

    Create hands-on-8-16.js

---
+++ hands-on-8-16.js
@@ -0,0 +1,48 @@
+/**
+ * Challenge: Create an advanced function.
+ * - Loop through backpackObjectArray to create an article with the class "backpack".
+ * - Give the article the ID of the current backpack object.
+ * - Set the inner HTML of the article to the existing HTML output provided in const content.
+ * - Append each backpack object to the <main> element.
+ */
+import backpackObjectArray from "./components/data.js";
+
+const backpackRender = backpackObjectArray.map((backpack) => {
+  let newBackpack = document.createElement('article')
+  newBackpack.classList.add("backpack");
+  newBackpack.setAttribute('id', backpack.id);
+
+newBack.innerHTML = `
+    <figure class="backpack__image">
+      <img src=${everydayPack.image} alt="" />
+    </figure>
+    <h1 class="backpack__name">${everydayPack.name}</h1>
+    <ul class="backpack__features">
+      <li class="packprop backpack__volume">Volume:<span> ${
+        everydayPack.volume
+      }l</span></li>
+      <li class="packprop backpack__color">Color:<span> ${
+        everydayPack.color
+      }</span></li>
+      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
+      <li class="packprop backpack__pockets">Number of pockets:<span> ${
+        everydayPack.pocketNum
+      }</span></li>
+      <li class="packprop backpack__strap">Left strap length:<span> ${
+        everydayPack.strapLength.left
+      } inches</span></li>
+      <li class="packprop backpack__strap">Right strap length:<span> ${
+        everydayPack.strapLength.right
+      } inches</span></li>
+      <li class="feature backpack__lid">Lid status:<span> ${
+        everydayPack.lidOpen ? "open" : "closed"
+      }</span></li>
+    </ul>
+`;
+  return newBackpack;
+});
+const main = document.querySelector(".maincontent");
+
+backpackRender.forEach((backpack) => {
+  main.append(backpack)
+})
