a0bb0dd8b6fe8153a41c66c678055428f2ab73ee
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-07-22 14:20:13 -0400

    Update hands-on-5-4.js

--- hands-on-5-4.js
+++ hands-on-5-4.js
@@ -1,5 +1,13 @@
+/**
+ * Note: This file is intentionally empty.
+ * You can use it to test your skills at traversing the DOM using JavaScript.
+ */
 
 
 let h1 = document.querySelector("h1");
 h1.innerHTML = "Hello"
 console.log(h1.textContent)
+ 
+let secondLi = document.querySelectorAll("ul li")[1];
+console.log(secondLi)
+secondLi.innerHTML = "Poo"
