142c1835610efe86c88d3f4c57505f724a1594fe
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-07-24 16:09:56 -0400

    Update hands-on-9-4-eventlistener.js

--- hands-on-9-4-eventlistener.js
+++ hands-on-9-4-eventlistener.js
@@ -6,57 +6,7 @@
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
 
-const gridContainer = document.querySelector('.grid');
-
-gridContainer.addEventListener('mouseenter', () => {
-    gridContainer.style.outline = '10px double pink'
-});
-
-gridContainer.addEventListener('mouseleave', () => {
-    gridContainer.style.outline = '';
-})
-
-const randColor = () => {
-    let hexColor = Math.floor(Math.random() * 16777215).toString(16);
-    return hexColor;
-}
-
-const gridCells = document.querySelectorAll('.cell');
-
-gridCells.forEach((cell) => {
-
-    cell.addEventListener('click', () => {
-        if (cell.style.outline) {
-            cell.style.outline = '';
-        } else {
-            cell.style.outline = `4px dotted #${randColor()}`;
-        }
-    })
-
-    cell.addEventListener('click', () => {
-        if (cell.style.backgroundColor) {
-            cell.style.backgroundColor = '';
-        } else {       
-            cell.style.backgroundColor = `#${randColor()}`;
-        }
-    })
-
-    cell.addEventListener('pointerover', () => {
-        cell.style.outline = "2px solid grey"/**
- * Practice: Play with event listeners
- * - Use an event listener and CSS either inline or through an added class to draw a highlight around the entire grid when you hover over it with your mouse.
- * - Add an event listener to each grid cell to highlight that cell when you hover your mouse over it.
- * - Add an event listener to each grid cell to change its background color when it is clicked.
- * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
- */
-
 const grid = document.querySelector('.grid');
 grid.addEventListener('mouseover', () => {
     grid.style.outline = '6px solid pink'
-})
-    })
-
-    cell.addEventListener('pointerleave', () => {
-        cell.style.outline = ""
-    })
 })
