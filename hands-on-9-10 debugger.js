7782ff083506214d415f016ce91c88054ff9e184
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-08-02 23:15:40 -0400

    Update hands-on-9-10 debugger.js

--- hands-on-9-10 debugger.js
+++ hands-on-9-10 debugger.js
@@ -1,2 +1,4 @@
 
-debugger --> sources --> page (select on the right) --> click line number --> refresh the browser --> enter 'F9' or hit '->.' on the right
+// debugger --> sources --> page (select on the right) --> click line number --> refresh the browser --> enter 'F9' or hit '->.' on the right
+
+// perfect for logic debugging
